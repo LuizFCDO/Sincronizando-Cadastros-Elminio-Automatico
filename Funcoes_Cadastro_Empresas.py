a2bfc6bd576a861c44a48fce018d89a1bc954aec
LuizFCDO <132104449+LuizFCDO@users.noreply.github.com>
2024-07-23 18:07:20 -0300

    Update Funcoes_Cadastro_Empresas.py
    Adicionando o código para compatibilidade do PC empresa e PC particular

--- Funcoes_Cadastro_Empresas.py
+++ Funcoes_Cadastro_Empresas.py
@@ -8,6 +8,9 @@ import signal # biblioteca para encerrar processos
 import pandas as pd
 from IPython.display import display # biblioteca com display para data fremes do pandas
 
+import PosicoesEmpresa # para importar as posições de ponteiro no computador da empresa
+#import PosicoesParticular # para importar as posições de ponteiro no computador particular
+
 def pidAreaRemota():
     for proc in ps.process_iter(): # Armazenando pid da área de trabalho remota no pidArea
         info = proc.as_dict(attrs=['pid', 'name'])
@@ -37,22 +40,6 @@ pat.PAUSE = 0.5
 pat.MINIMUM_DURATION = 0.25
 
 dataParaSalvamento = time.strftime("%d %m %Y", time.localtime())
-
-cord = {'cigo atalho':(46,351), # Cigo - Atalho 
-        'pesq item 4 cat':(793,198), # Dentro do pesquisa item, selecionar categoria da quarta pesquisa
-        'cod. ref pesq item 4 cat':(754,284), # Selecionar 'Cod. ref' no item anterior
-        'lupa pesq item':(937,211), # Lupa de pesquisa no pesquisa item
-        'itens pesq item':(921,449), # Janela de itens depois de busca
-        'min area rem':(1209,14), # Minimizar Área Remota
-        'prod 4 cat':(780,201), # Dentro do Produto, selecionar categoria na quarta pesquisa
-        'cod. ref prod 4 cat':(741,304), # Selecionar 'Cod. ref' no item anterior
-        'lupa prod':(917,206), # Lupa de pesquisa no produto
-        'empr prod':(453,938), # Selecionar empresa produto dentro de produto, depois de item pesquisado
-        'alt empr prod':(119,994), # Alterar empresa produto 
-        'grup alt empr prod':(270,322), # Alterando empresa produto, grupo
-        'cancel alt empr prod':(50,293), # Cancelar, dentro de alterar empresa produto
-        'adic empr prod':(119,970) # Adicionar empresa produto
-        }
 
 empr = {
     'elm' : 'ELMINIO',
