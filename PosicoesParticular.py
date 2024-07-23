3a12b9f58834e38aa920956d3b3e0fea52019432
LuizFCDO <luizfelipe.c.d.o@gmail.com>
2024-07-23 17:34:42 -0300

    Criando arquivo com posições de ponteiro para pc particular e pc da empresa

---
+++ PosicoesParticular.py
@@ -0,0 +1,15 @@
+cord = {'cigo atalho':(45,337), # Cigo - Atalho 
+        'pesq item 4 cat':(805,198), # Dentro do pesquisa item, selecionar categoria da quarta pesquisa
+        'cod. ref pesq item 4 cat':(760,288), # Selecionar 'Cod. ref' no item anterior
+        'lupa pesq item':(932,210), # Lupa de pesquisa no pesquisa item
+        'itens pesq item':(947,388), # Janela de itens depois de busca
+        'min area rem':(1206,17), # Minimizar Área Remota
+        'prod 4 cat':(766,199), # Dentro do Produto, selecionar categoria na quarta pesquisa
+        'cod. ref prod 4 cat':(740,310), # Selecionar 'Cod. ref' no item anterior
+        'lupa prod':(916,217), # Lupa de pesquisa no produto
+        'empr prod':(457,822), # Selecionar empresa produto dentro de produto, depois de item pesquisado
+        'alt empr prod':(121,869), # Alterar empresa produto 
+        'grup alt empr prod':(280,322), # Alterando empresa produto, grupo
+        'cancel alt empr prod':(45,287), # Cancelar, dentro de alterar empresa produto
+        'adic empr prod':(119,849) # Adicionar empresa produto
+        }
