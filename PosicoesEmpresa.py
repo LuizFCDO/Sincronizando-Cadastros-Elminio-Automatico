4daa1634c668bcaa621644d914fa21822bbf90b6
LuizFCDO <luizfelipe.c.d.o@gmail.com>
2024-07-23 18:17:38 -0300

    Atualizando Posições Empresa

--- PosicoesEmpresa.py
+++ PosicoesEmpresa.py
@@ -1,1 +1,15 @@
-#ainda nada
+cord = {'cigo atalho':(46,351), # Cigo - Atalho 
+        'pesq item 4 cat':(793,198), # Dentro do pesquisa item, selecionar categoria da quarta pesquisa
+        'cod. ref pesq item 4 cat':(754,284), # Selecionar 'Cod. ref' no item anterior
+        'lupa pesq item':(937,211), # Lupa de pesquisa no pesquisa item
+        'itens pesq item':(921,449), # Janela de itens depois de busca
+        'min area rem':(1209,14), # Minimizar Área Remota
+        'prod 4 cat':(780,201), # Dentro do Produto, selecionar categoria na quarta pesquisa
+        'cod. ref prod 4 cat':(741,304), # Selecionar 'Cod. ref' no item anterior
+        'lupa prod':(917,206), # Lupa de pesquisa no produto
+        'empr prod':(453,938), # Selecionar empresa produto dentro de produto, depois de item pesquisado
+        'alt empr prod':(119,994), # Alterar empresa produto 
+        'grup alt empr prod':(270,322), # Alterando empresa produto, grupo
+        'cancel alt empr prod':(50,293), # Cancelar, dentro de alterar empresa produto
+        'adic empr prod':(119,970) # Adicionar empresa produto
+        }
